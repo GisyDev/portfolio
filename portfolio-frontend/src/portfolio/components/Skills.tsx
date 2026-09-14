2c15da2f6cb9065ff1811f840001aea1bafd56e8
GisyDev <gisyestevez@gmail.com>
2026-09-14 19:12:40 +0100

    Estilo de skills y mejora de imagenes añadida

--- portfolio-frontend/src/portfolio/components/Skills.tsx
+++ portfolio-frontend/src/portfolio/components/Skills.tsx
@@ -8,22 +8,22 @@ const Skills = () => {
 
     const folderImages = "language_icons"
 
-    const [emblaRef] = useEmblaCarousel({ 
+    const [emblaRef] = useEmblaCarousel({
         loop: true,
         dragFree: true
     }, [AutoScroll({ playOnInit: true, speed: 1 })])
 
     return (
         <section className='w-7xl m-auto space-y-7'>
-            <h1 className='font-bold text-4xl'>Tecnologías</h1>
+            <h1 className='font-bold text-4xl text-primary'>Tecnologías</h1>
             <div className="overflow-hidden">
                 <div className="overflow-hidden" ref={emblaRef}>
-                    <div className="flex touch-pan-y touch-pinch-zoom gap-3 ">
+                    <div className="flex touch-pan-y touch-pinch-zoom gap-5 ">
                         {
                             skillsSlider.map((skill) => {
-                                return <div className="w-min flex-none basis-1/6 last:mr-3 items-center rounded-3xl border-2 p-3" key={skill.name}>
-                                    <img className='w-30 m-auto' src={getImage(folderImages, skill.image)} alt="" />
-                                    <p className='text-center'>{skill.name}</p>
+                                return <div className="flex-none basis-1/5 last:mr-3 items-center rounded-lg p-3 bg-bg-card border-t-4 border-primary" key={skill.name}>
+                                    <img className='w-30 m-auto p-3' src={getImage(folderImages, skill.image)} alt="" />
+                                    <p className='text-2xl text-center mt-5 font-semibold'>{skill.name}</p>
                                 </div>
                             })
                         }
