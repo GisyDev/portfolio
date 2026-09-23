6066d6a3f7c685497c435eab6251fd0d8a08f9ac
Gisy7 <gisyestevezfp@gmail.com>
2026-09-23 20:51:34 +0100

    Añadido el estilo provisional de dispinible trabajar en about

--- portfolio-frontend/src/portfolio/ui/About.tsx
+++ portfolio-frontend/src/portfolio/ui/About.tsx
@@ -31,7 +31,7 @@ const About = () => {
                                 <h1 className='text-primary-text animate-fade-in animate-delay-25 text-sm flex flex-col space-y-2'>
                                     <div className='flex gap-1 text-sm sm:text-base'>
                                         <p className='text-primary'>{">"}</p>
-                                        <p className='text-primary-text sm:text-md'>{"Hola, soy "}</p>
+                                        <p className='text-primary-text sm:text-md'>{"Hola, soy"}</p>
                                     </div>
                                     <span className='text-white font-bold text-3xl sm:text-4xl md:text-4xl'> {name}</span>
                                 </h1>
@@ -42,15 +42,15 @@ const About = () => {
                         </div>
 
                         <div className='flex items-center gap-3 animate-fade-in animate-delay-200 mt-4'>
-                            <PrimaryButton className='bg-primary hover:bg-primary-hover '>
+                            <PrimaryButton className='bg-primary hover:bg-primary-hover'>
                                 <i className="fa-solid fa-download"></i>
                                 {/* fa-lg */}
                                 <p className=''>Descargar CV</p>
                             </PrimaryButton>
 
                             <PrimaryButton className='bg-gray-500 hover:bg-gray-500/90' onClick={() => navigate("SobreMi")}>
-                                <i className="fa-solid fa-circle-info fa-lg"></i>
                                 <p className=''>Más información</p>
+                                <i className="fa-solid fa-arrow-right-long"></i>
                             </PrimaryButton>
                         </div>
 
@@ -72,14 +72,17 @@ const About = () => {
                     </div>
                 </div>
                 <div className='flex animate-fade-in animate-delay-150 '>
-                    <div className='flex gap-8 items-center'>
+                    <div className='flex md:flex-col justify-center gap-8 items-center'>
                         <img src={cara} alt="Yo" className='rounded-full text-primary-button ring-offset-2 ring-offset-black ring-2 ring-primary object-cover size-30 sm:size-35 md:size-56' />
                         <div>
-                            <button className='text-sm bg-primary self-start p-2 rounded-lg md:hidden'>
-                                <p>Disponible para trabajar</p>
+                            <button className="group flex items-center gap-2 text-xs px-4 py-2 rounded-full text-white border border-primary/50 bg-primary/10 backdrop-blur-sm  hover:border-primary transition-all duration-300">
+                                <span className="relative flex size-2">
+                                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"></span>
+                                    <span className="relative inline-flex rounded-full size-2 bg-primary"></span>
+                                </span>
+                                <p className="font-medium tracking-wide">Disponible para trabajar</p>
                             </button>
                         </div>
-
                     </div>
                 </div>
             </article>
